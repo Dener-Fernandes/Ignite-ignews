d7bb5e803743488f6694fb841dd4043b573bf277
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2022-02-06 18:11:19 -0400

    Feature de autenticação concluída

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,23 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session} = useSession();
 
-  return isUserLoggedIn ? (
+  console.log("session", session);
+
+  return session ? (
     <button
     className={styles.signInButton} 
     type="button"
+    onClick={() => signOut()}
     >
       <FaGithub color="#04D361"/>
-      Dener Fernandes
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
       <button
         className={styles.signInButton} 
         type="button"
+        onClick={() => signIn('github')}
       >
         <FaGithub color="#EBA417"/>
         Sign in with Github
